e4947ebed69b70d15d58aa224102e68d7fdfec50
proohit <direnc99@gmail.com>
2021-09-12 20:48:59 +0200

    added register anilist token callback

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const Koa = require('koa');
+const fs = require('fs');
 const Router = require('koa-router');
 const body = require('koa-body');
 const PlexWebHookEvent = require('./PlexWebHookEvent');
@@ -20,6 +21,43 @@ router.post('/webhook', async (ctx) => {
   } catch (error) {
     logger.error(error.toString());
   }
+});
+
+router.get('/anilist-callback', (ctx) => {
+  ctx.body = fs.createReadStream('redirect.html');
+  ctx.type = 'html';
+  ctx.res.statusCode = 200;
+});
+
+router.post('/anilist-callback', (ctx) => {
+  const { plexName, access_token } = ctx.request.body;
+  ctx.response.status = 400;
+  ctx.response.type = 'text/plain';
+  ctx.response.body = 'Unknown error occured';
+  if (!access_token) {
+    ctx.response.body =
+      'Missing access token. Visit https://anilist.co/api/v2/oauth/authorize?client_id={clientID}&response_type=token to get a token';
+    return;
+  }
+  if (!plexName) {
+    ctx.response.body = 'Missing plex name.';
+    return;
+  }
+  const config = JSON.parse(fs.readFileSync('config.json'));
+  const newUser = { plexName, anilistToken: access_token };
+  let newUsers = [...config['anilistUsers']];
+  if (newUsers.find((user) => user.plexName === plexName)) {
+    newUsers = newUsers.filter((user) => user.plexName !== plexName);
+    newUsers.push(newUser);
+    ctx.response.status = 201;
+    ctx.response.body = 'Your user has been updated';
+  } else {
+    newUsers.push(newUser);
+    ctx.response.status = 201;
+    ctx.response.body = 'You have been added to the plexilist hook';
+  }
+  config['anilistUsers'] = newUsers;
+  fs.writeFileSync('config.json', JSON.stringify(config));
 });
 
 app.use(router.routes());
