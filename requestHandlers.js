57ff4916dd87e3f291a33b7d0f81343d7e4afc65
proohit <direnc99@gmail.com>
2022-09-08 20:40:23 +0200

    feat: support anime movies

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,8 +1,8 @@
-const fs = require('fs');
-const PlexWebHookEvent = require('./PlexWebHookEvent');
-const logger = require('./logger');
-const { handleScrobble, handleRate } = require('./eventHandlers');
-let config = require('./config.json');
+const fs = require("fs");
+const PlexWebHookEvent = require("./PlexWebHookEvent");
+const logger = require("./logger");
+const { handleScrobble, handleRate } = require("./eventHandlers");
+let config = require("./config.json");
 
 async function handleWebhook(ctx) {
   try {
@@ -19,72 +19,85 @@ async function handleWebhook(ctx) {
 }
 
 function shouldHandleScrobble(event) {
-  if (event.event !== 'media.scrobble') {
+  if (event.event !== "media.scrobble") {
     return false;
   }
-  if (event.Metadata.type !== 'episode') {
+
+  const isEpisode = event.Metadata.type === "episode";
+  const isMovie = event.Metadata.type === "movie";
+  if (!isEpisode && !isMovie) {
     return false;
   }
-  if (event.Metadata.librarySectionTitle !== 'Anime') {
+
+  const isFromMovieLibrary =
+    event.Metadata.librarySectionTitle === "Anime Movies";
+  const isFromSeriesLibrary = event.Metadata.librarySectionTitle === "Anime";
+
+  if (!isFromMovieLibrary && !isFromSeriesLibrary) {
     return false;
   }
+
   return true;
 }
 
 function shouldHandleRate(event) {
-  if (event.event !== 'media.rate') {
+  if (event.event !== "media.rate") {
     return false;
   }
-  if (event.Metadata.librarySectionTitle !== 'Anime') {
+  const isFromMovieLibrary =
+    event.Metadata.librarySectionTitle === "Anime Movies";
+  const isFromSeriesLibrary = event.Metadata.librarySectionTitle === "Anime";
+
+  if (!isFromMovieLibrary && !isFromSeriesLibrary) {
     return false;
   }
   return true;
 }
 
 async function anilistRedirect(ctx) {
-  ctx.body = fs.createReadStream('redirect.html');
-  ctx.type = 'html';
+  ctx.body = fs.createReadStream("redirect.html");
+  ctx.type = "html";
   ctx.res.statusCode = 200;
 }
 
 async function anilistLogin(ctx) {
-  let html = fs.readFileSync('anilist-login.html', { encoding: 'utf-8' });
-  html = html.replace('{#clientId}', config.clientId);
+  let html = fs.readFileSync("anilist-login.html", { encoding: "utf-8" });
+  html = html.replace("{#clientId}", config.clientId);
   ctx.body = html;
-  ctx.type = 'html';
+  ctx.type = "html";
   ctx.res.statusCode = 200;
 }
 
 async function addNewAnilistUser(ctx) {
   const { plexName, access_token } = ctx.request.body;
   ctx.response.status = 400;
-  ctx.response.type = 'text/plain';
-  ctx.response.body = 'Unknown error occured';
+  ctx.response.type = "text/plain";
+  ctx.response.body = "Unknown error occured";
   if (!access_token) {
     ctx.response.body =
-      'Missing access token. Visit https://anilist.co/api/v2/oauth/authorize?client_id={clientID}&response_type=token to get a token';
+      "Missing access token. Visit https://anilist.co/api/v2/oauth/authorize?client_id={clientID}&response_type=token to get a token";
     return;
   }
   if (!plexName) {
-    ctx.response.body = 'Missing plex name.';
+    ctx.response.body = "Missing plex name.";
     return;
   }
   const newUser = { plexName, anilistToken: access_token };
-  let newUsers = [...config['anilistUsers']];
+  let newUsers = [...config["anilistUsers"]];
   if (newUsers.find((user) => user.plexName === plexName)) {
     newUsers = newUsers.filter((user) => user.plexName !== plexName);
     newUsers.push(newUser);
     ctx.response.status = 201;
-    ctx.response.body = 'Your user has been updated';
+    ctx.response.body = "Your user has been updated";
   } else {
     newUsers.push(newUser);
     ctx.response.status = 201;
-    ctx.response.body = 'You have been added to the plexilist hook';
+    ctx.response.body = "You have been added to the plexilist hook";
   }
-  config['anilistUsers'] = newUsers;
-  fs.writeFileSync('config.json', JSON.stringify(config));
-  delete require.cache[require.resolve('./config.json')];
-  config = require('./config.json');
+  config["anilistUsers"] = newUsers;
+  fs.writeFileSync("config.json", JSON.stringify(config));
+  delete require.cache[require.resolve("./config.json")];
+  config = require("./config.json");
 }
 
 module.exports = {
